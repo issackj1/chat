08d4eec868481a328f63e25adc860e3a35bff7e2
issackj1 <issack.john1@ucalgary.ca>
2019-03-10 21:26:21 -0600

    somewhat done

--- public/index.js
+++ public/index.js
@@ -2,32 +2,72 @@ let app = require('express')();
 let express = require('express');
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+let cookieParser = require('cookie-parser')
+
 let users = [];
+let history = [];
+let date = new Date();
 
 app.use(express.static('public/'))
+app.use(cookieParser ())
+
+app.get('/', function(req, res) {
+  res.cookie("My First Cookie", "Yeah!");
+});
 
 io.on('connection', function (socket) {
   socket.nickname = ID();
-  console.log(socket.nickname + ' connected');
-  users.push(socket.nickname);
-  io.emit('update users', users);
-  io.emit('init-name', socket.nickname);
+  socket.color = "black";
+  users.push({
+    name: socket.nickname,
+    color: socket.color
+  })
+  //console.log(socket.nickname + ' connected' + socket.color);
 
-  socket.on('send-nickname', function (nickname) {
-    if (!users.includes(nickname)) {
-      let index = users.indexOf(socket.nickname);
-      users[index] = nickname;
+  io.emit('update users', users);
+  socket.emit('init-name', socket.nickname);
+  socket.emit('update history', history);
+
+  socket.on('/nick', function (nickname) {
+    let exist = false;
+    for (i = 0; i < users.length; i++) {
+      if(users[i].name === nickname) { 
+        exist = true; 
+        break;
+      }
+    }
+    if (!exist) {
+      for (i = 0; i < users.length; i++) {
+        if(users[i].name === socket.nickname){
+          users[i].name = nickname;
+        }
+      }
       socket.nickname = nickname;
+      socket.emit('init-name', socket.nickname);
       io.emit('update users', users);
     }
+  })
+
+  socket.on('change color', function (color) {
+    for (i = 0; i < users.length; i++) {
+      if(users[i].name === socket.nickname){
+        users[i].color = color;
+      }
+    }
+    io.emit('update users', users);
   })
 
   socket.on('disconnect', function () {
     console.log('a user disconnected');
     let temp = users;
     users = [];
-    for (let value of temp) {
-      if (value !== socket.nickname) { users.push(value) }
+    for (i = 0; i < temp.length; i++) {
+      if(!(temp[i].name === socket.nickname)){
+        users.push({
+          name: temp[i].name,
+          color: temp[i].color
+        });
+    }
     }
     io.emit('update users', users);
 
@@ -37,8 +77,12 @@ io.on('connection', function (socket) {
     console.log('message: ' + msg + socket.nickname);
   });
 
-  socket.on('chat message', function (msg) {
-    io.emit('chat message', msg, socket.nickname);
+  socket.on('chat message', function (msg, color) {
+    let minute = date.getMinutes();
+    if (minute < 10) { minute = '0' + date.getMinutes() }
+    history.push('(' + date.getHours() + ":" + minute + ')' + "\xa0\xa0" + socket.nickname + ":\xa0\xa0" + msg);
+    socket.broadcast.emit('chat message', msg, '<span style=color:' + color + '>' + socket.nickname + '</span>');
+    socket.emit('chat message', msg, '<b><span style=color:' + color + '>' + socket.nickname + '</span></b>');
   });
 
 });
